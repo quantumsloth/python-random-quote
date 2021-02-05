21284789cd387188bb12241210f1cf908a47ef2c
quantumsloth <78463142+quantumsloth@users.noreply.github.com>
2021-02-04 18:36:31 -0800

    Create get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -5,9 +5,9 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-last = 13
-rnd = random.randint(0, last)
-  print(quotes[rnt])
+  last = 13
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
